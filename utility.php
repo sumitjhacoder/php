013cb50655de6c4c17527f310791391326a6eccf
sumitjhacoder <70857738+sumitjhacoder@users.noreply.github.com>
2021-02-16 13:31:20 +0530

    Create utility.php

---
+++ utility.php
@@ -0,0 +1,241 @@
+// Link image type to correct image loader and saver
+// - makes it easier to add additional types later on
+// - makes the function easier to read
+const IMAGE_HANDLERS = [
+    IMAGETYPE_JPEG => [
+        'load' => 'imagecreatefromjpeg',
+        'save' => 'imagejpeg',
+        'quality' => 100
+    ],
+    IMAGETYPE_PNG => [
+        'load' => 'imagecreatefrompng',
+        'save' => 'imagepng',
+        'quality' => 0
+    ],
+    IMAGETYPE_GIF => [
+        'load' => 'imagecreatefromgif',
+        'save' => 'imagegif'
+    ]
+];
+
+/**
+ * @param $src - a valid file location
+ * @param $dest - a valid file target
+ * @param $targetWidth - desired output width
+ * @param $targetHeight - desired output height or null
+ */
+function createThumbnail($src, $dest, $targetWidth, $targetHeight = null)
+{
+
+    // 1. Load the image from the given $src
+    // - see if the file actually exists
+    // - check if it's of a valid image type
+    // - load the image resource
+
+    // get the type of the image
+    // we need the type to determine the correct loader
+    $type = exif_imagetype($src);
+
+    // if no valid type or no handler found -> exit
+    if (!$type || !IMAGE_HANDLERS[$type]) {
+        return null;
+    }
+
+    // load the image with the correct loader
+    $image = call_user_func(IMAGE_HANDLERS[$type]['load'], $src);
+
+    // no image found at supplied location -> exit
+    if (!$image) {
+        return null;
+    }
+
+
+    // 2. Create a thumbnail and resize the loaded $image
+    // - get the image dimensions
+    // - define the output size appropriately
+    // - create a thumbnail based on that size
+    // - set alpha transparency for GIFs and PNGs
+    // - draw the final thumbnail
+
+    // get original image width and height
+    $width = imagesx($image);
+    $height = imagesy($image);
+
+    // maintain aspect ratio when no height set
+    if ($targetHeight == null) {
+
+        // get width to height ratio
+        $ratio = $width / $height;
+
+        // if is portrait
+        // use ratio to scale height to fit in square
+        if ($width > $height) {
+            $targetHeight = floor($targetWidth / $ratio);
+        }
+        // if is landscape
+        // use ratio to scale width to fit in square
+        else {
+            $targetHeight = $targetWidth;
+            $targetWidth = floor($targetWidth * $ratio);
+        }
+    }
+
+    // create duplicate image based on calculated target size
+    $thumbnail = imagecreatetruecolor($targetWidth, $targetHeight);
+
+    // set transparency options for GIFs and PNGs
+    if ($type == IMAGETYPE_GIF || $type == IMAGETYPE_PNG) {
+
+        // make image transparent
+        imagecolortransparent(
+            $thumbnail,
+            imagecolorallocate($thumbnail, 0, 0, 0)
+        );
+
+        // additional settings for PNGs
+        if ($type == IMAGETYPE_PNG) {
+            imagealphablending($thumbnail, false);
+            imagesavealpha($thumbnail, true);
+        }
+    }
+
+    // copy entire source image to duplicate image and resize
+    imagecopyresampled(
+        $thumbnail,
+        $image,
+        0,
+        0,
+        0,
+        0,
+        $targetWidth,
+        $targetHeight,
+        $width,
+        $height
+    );
+
+
+    // 3. Save the $thumbnail to disk
+    // - call the correct save method
+    // - set the correct quality level
+
+    // save the duplicate version of the image to disk
+    return call_user_func(
+        IMAGE_HANDLERS[$type]['save'],
+        $thumbnail,
+        $dest,
+        IMAGE_HANDLERS[$type]['quality']
+    );
+}
+// Using this script you can now for example generate square 160 by 160 pixel thumbnails with the following command.
+
+createThumbnail('profile.jpg', 'profile_thumb.jpg', 160);
+
+
+/**
+ * clean array data before insert to db
+ */
+if (!function_exists('mysql_clean_array')) {
+    function mysql_clean_array(&$data)
+    {
+        array_walk_recursive($data, function (&$val, $key) {
+            $val = mysql_real_escape_string($val);
+        });
+    }
+}
+// clean data 
+// $request = $_REQUEST;
+// mysql_clean_array($request); // use $request instead of $_REQUEST
+
+if (!function_exists('fileLog')) {
+    function fileLog($msg, $fileName = '')
+    {
+        $fileName = ($fileName) ? $fileName : "log-file.txt";
+        $logFile = fopen($fileName, "a+") or die("Unable to open file!");
+        fwrite($logFile, $msg);
+        fclose($logFile);
+    }
+}
+
+function numberTowords($num)
+{
+
+    $ones = array(
+        0 => "ZERO",
+        1 => "ONE",
+        2 => "TWO",
+        3 => "THREE",
+        4 => "FOUR",
+        5 => "FIVE",
+        6 => "SIX",
+        7 => "SEVEN",
+        8 => "EIGHT",
+        9 => "NINE",
+        10 => "TEN",
+        11 => "ELEVEN",
+        12 => "TWELVE",
+        13 => "THIRTEEN",
+        14 => "FOURTEEN",
+        15 => "FIFTEEN",
+        16 => "SIXTEEN",
+        17 => "SEVENTEEN",
+        18 => "EIGHTEEN",
+        19 => "NINETEEN",
+        "014" => "FOURTEEN"
+    );
+    $tens = array(
+        0 => "ZERO",
+        1 => "TEN",
+        2 => "TWENTY",
+        3 => "THIRTY",
+        4 => "FORTY",
+        5 => "FIFTY",
+        6 => "SIXTY",
+        7 => "SEVENTY",
+        8 => "EIGHTY",
+        9 => "NINETY"
+    );
+    $hundreds = array(
+        "HUNDRED",
+        "THOUSAND",
+        "MILLION",
+        "BILLION",
+        "TRILLION",
+        "QUARDRILLION"
+    ); /*limit t quadrillion */
+    $num = number_format($num, 2, ".", ",");
+    $num_arr = explode(".", $num);
+    $wholenum = $num_arr[0];
+    $decnum = $num_arr[1];
+    $whole_arr = array_reverse(explode(",", $wholenum));
+    krsort($whole_arr, 1);
+    $rettxt = "";
+    foreach ($whole_arr as $key => $i) {
+
+        while (substr($i, 0, 1) == "0")
+            $i = substr($i, 1, 5);
+        if ($i < 20) {
+            /* echo "getting:".$i; */
+            $rettxt .= $ones[$i];
+        } elseif ($i < 100) {
+            if (substr($i, 0, 1) != "0")  $rettxt .= $tens[substr($i, 0, 1)];
+            if (substr($i, 1, 1) != "0") $rettxt .= " " . $ones[substr($i, 1, 1)];
+        } else {
+            if (substr($i, 0, 1) != "0") $rettxt .= $ones[substr($i, 0, 1)] . " " . $hundreds[0];
+            if (substr($i, 1, 1) != "0") $rettxt .= " " . $tens[substr($i, 1, 1)];
+            if (substr($i, 2, 1) != "0") $rettxt .= " " . $ones[substr($i, 2, 1)];
+        }
+        if ($key > 0) {
+            $rettxt .= " " . $hundreds[$key] . " ";
+        }
+    }
+    if ($decnum > 0) {
+        $rettxt .= " and ";
+        if ($decnum < 20) {
+            $rettxt .= $ones[$decnum];
+        } elseif ($decnum < 100) {
+            $rettxt .= $tens[substr($decnum, 0, 1)];
+            $rettxt .= " " . $ones[substr($decnum, 1, 1)];
+        }
+    }
+    return $rettxt;
+}
