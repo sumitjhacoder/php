ab7881ea50d0cae135a79455d755850772cf11eb
sumitjhacoder <70857738+sumitjhacoder@users.noreply.github.com>
2021-03-19 00:12:26 +0530

    Create Installation_cmd.php

---
+++ laravel/Installation_cmd.php
@@ -0,0 +1,7 @@
+E:\>cd xampp
+E:\xampp>cd htdocs
+//E:\xampp\htdocs>composer global remove laravel/installer
+E:\xampp\htdocs>composer global require laravel/installer
+E:\xampp\htdocs>composer create-project --prefer-dist laravel/laravel example
+E:\xampp\htdocs>composer install
+E:\xampp\htdocs\example>php artisan serve
